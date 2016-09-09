bcc72ae495753b55a448b768a3a901285a6ec533
Alexis Padula <apadula2@mooney202-29.elon.edu>
2016-09-09 14:31:03 -0400

    first app

--- Mealtracker/ViewController.swift
+++ Mealtracker/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func setLabelText(_ sender: UIButton){
+        mealNameLabel.text = nameTextField.text
+    
     }
+
+    
 
 
 }
