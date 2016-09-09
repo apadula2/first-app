cff919e0afdccec8bbe60fed2b071729a1b9a040
Elon University <apadula2@mooney202-29.elon.edu>
2016-09-09 13:53:01 -0400

    Initial Commit

---
+++ Mealtracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mealtracker
+//
+//  Created by Alexis Padula on 9/9/16.
+//  Copyright © 2016 Alexis Padula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
